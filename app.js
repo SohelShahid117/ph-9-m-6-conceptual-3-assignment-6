7de5f35885781bc4a4d00b81900e243461ee53ce
SohelShahid117 <sohelshahid09@gmail.com>
2024-03-14 23:45:04 +0600

    complete 30 min

--- app.js
+++ app.js
@@ -1,10 +1,58 @@
 // alert('hi')
+//giasVaiGitub:https://github.com/gias-uddin-swe/B9-API-Solved?tab=readme-ov-file
+/*
+API
+        LatestPosts: - https://openapi.programming-hero.com/api/retro-forum/latest-posts
 
-const loadPost = () =>{
-    const url = `https://openapi.programming-hero.com/api/retro-forum/posts`;
-    console.log(url);
+        AllPosts: - https://openapi.programming-hero.com/api/retro-forum/posts
+
+        PostSearchByQuery
+        PostByQuery: - https://openapi.programming-hero.com/api/retro-forum/posts?category=categoryName
+
+        Example:  
+        PostByQuery: - https://openapi.programming-hero.com/api/retro-forum/posts?category=coding
+*/
+
+// const loadPost = () =>{
+//     const url = `https://openapi.programming-hero.com/api/retro-forum/posts`;
+//     console.log(url);
+// }
+// loadPost();
+
+const loadPost = async (qry='') =>{
+    const url = `https://openapi.programming-hero.com/api/retro-forum/posts?${qry}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    console.log(data.posts);
 }
-loadPost();
+const handleSearch = () =>{
+    const searchBoxValue = document.getElementById('search-box').value;
+    console.log(searchBoxValue)
+    loadPost(`category=${searchBoxValue}`);
+}
 
-//7min 
-console.log(7)
+// loadPost();
+
+const loadAllProducts= async (qry='') => {
+    const url = `https://fakestoreapi.com/products${qry}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+
+    data.forEach((itm)=>{
+        console.log(itm);
+    })
+}
+
+const itemWiseProduct=()=>{
+    // https://fakestoreapi.com/products/category/jewelery
+    // loadAllProducts(`/category/jewelery`)
+    // loadAllProducts(`/category/electronics`)
+    loadAllProducts();
+}
+itemWiseProduct()
+// loadAllProducts()
+
+//30 min
+
