89f2c78dc79d1455d766c062338e4e8956db6c87
SohelShahid117 <sohelshahid09@gmail.com>
2024-03-15 07:07:35 +0600

    start from 45 min

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ API
 // }
 // loadPost();
 
+/*
 const loadPost = async (qry='') =>{
     const url = `https://openapi.programming-hero.com/api/retro-forum/posts?${qry}`;
     const res = await fetch(url);
@@ -31,6 +32,7 @@ const handleSearch = () =>{
     console.log(searchBoxValue)
     loadPost(`category=${searchBoxValue}`);
 }
+*/
 
 // loadPost();
 
@@ -40,10 +42,65 @@ const loadAllProducts= async (qry='') => {
     const data = await res.json();
     console.log(data);
 
+    const productContainer = document.getElementById('product-container');
+    productContainer.innerHTML = ' ';
+
     data.forEach((itm)=>{
+        const div = document.createElement('div');
+        // card w-96 bg-base-100 shadow-xl"
+        div.classList.add('card');
+        div.classList.add('w-96');
+        div.classList.add('bg-base-100');
+        div.classList.add('shadow-xl');
+        div.classList.add('mt-5');
+        div.innerHTML = `
+
+          <figure class="px-10 pt-10" >
+                    <img src='${itm.image}' class="rounded-xl h-60">
+          </figure>
+          <div class="card-body items-center text-center">
+            <h2 class="card-title">${itm.title}</h2>
+            <p>${itm.description}</p>
+            <div class="card-actions">
+                <h4>Price:${itm.price}</h4>
+              <button class="btn btn-primary">${itm.category}</button>
+            </div>
+            <hr>
+            <div class="price-container">
+
+                <div class="stats shadow">
+                    <div class="stat">
+                        <div class="stat-figure text-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                        </div>
+                        <div class="stat-title">Total Likes</div>
+                        <div class="stat-value text-primary">25.6K</div>
+                        <div class="stat-desc">21% more than last month</div>
+                    </div>
+                </div>
+
+                <div class="img-container rating">
+                    <input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" checked />450
+                </div>
+
+                <button class="btn btn-success">add-to-cart</button>
+                
+            </div>
+        
+        `
         console.log(itm);
+        productContainer.appendChild(div);
     })
+
 }
+
+const handleSearch = () =>{
+    const searchBoxValue = document.getElementById('search-box').value;
+    console.log(searchBoxValue)
+    loadAllProducts(`/category/${searchBoxValue}`);
+}
+
+/*
 
 const itemWiseProduct=()=>{
     // https://fakestoreapi.com/products/category/jewelery
@@ -53,6 +110,7 @@ const itemWiseProduct=()=>{
 }
 itemWiseProduct()
 // loadAllProducts()
+*/
 
-//30 min
 
+//45 min
